0c4808acb6d8da33eb487227fac0d24c27027f61
mightygaby <gaby.rf@gmail.com>
2017-04-20 11:53:44 -0500

    basic bubbles

---
+++ scripts.js
@@ -0,0 +1,88 @@
+// TO DO:
+// stop animation when hidden
+// add reset button
+
+$(document).ready(function() {
+    setContainerSize('#background-wrap');
+    if (!("autofocus" in document.createElement("textarea"))) {
+      document.getElementById("element").focus();
+    }
+});
+
+function encapsulate(){
+  var fear = $('textarea');
+  var val = fear.val() ? fear.val() : "Your Anxieties & Fears";
+  $('.fear').text(val);
+  fear.val('');
+  hideContent();
+  $('.bubble').removeClass('hidden');
+  animateDiv();
+}
+
+function explode(e){
+  $('.bubble').hide();
+  displayQuote();
+}
+
+function displayQuote(){
+  var container=$('.quotes'),
+      quotes = ["this too shall pass"];
+  var rand = Math.floor(Math.random()*(quotes.length-0)+0)
+  console.log(quotes[rand])
+  container.removeClass('hidden');
+  container.val(quotes[rand]);
+
+}
+function hideContent () {
+  $('.content > p, button, textarea').hide()
+}
+
+function setContainerSize(container) {
+  var w = window.innerWidth,
+      h = window.innerHeight;
+  $(container).width(w).height(h);
+}
+
+function makeNewPosition($container) {
+
+    // Get viewport dimensions (remove the dimension of the div)
+    $container = ($container || $(window))
+    var h = $container.height() - 50;
+    var w = $container.width() - 50;
+
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh, nw];
+
+}
+
+function animateDiv() {
+    var $target = $('.bubble');
+    var newq = makeNewPosition($target.parent()),
+        oldq = $target.offset();
+    var speed = calcSpeed([oldq.top, oldq.left], newq);
+
+    $('.bubble').animate({
+        top: newq[0],
+        left: newq[1]
+    }, speed, function() {
+        animateDiv();
+    });
+
+};
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
